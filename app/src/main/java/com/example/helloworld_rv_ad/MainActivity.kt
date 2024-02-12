61e2b64888b5478beef65363235d82f06253e6fb
rodrigo.valiente <rodrigo.valiente@alumnos.upm.es>
2024-02-12 12:34:02 +0100

    boton

--- app/src/main/java/com/example/helloworld_rv_ad/MainActivity.kt
+++ app/src/main/java/com/example/helloworld_rv_ad/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.helloworld_rv_ad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonNext: Button = findViewById(R.id.mainToSecondButton)
+        buttonNext.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
