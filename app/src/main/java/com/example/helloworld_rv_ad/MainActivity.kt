5678c4372fa883d7991efbbecb8c0989ddea6764
rodrigo.valiente <rodrigo.valiente@alumnos.upm.es>
2024-02-13 16:11:49 +0100

    gps coordinates added for week 6

--- app/src/main/java/com/example/helloworld_rv_ad/MainActivity.kt
+++ app/src/main/java/com/example/helloworld_rv_ad/MainActivity.kt
@@ -5,9 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
     private val TAG = "btaMainActivity"
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,8 +32,51 @@ class MainActivity : AppCompatActivity() {
             val intent2 = Intent(this, ThirdActivity::class.java)
             startActivity(intent2)
         }
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // Check for location permissions
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                locationPermissionCode
+            )
+        } else {
+            // The location is updated every 5000 milliseconds (or 5 seconds) and/or if the device moves more than 5 meters,
+            // whichever happens first
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+        }
     }
-}
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+                }
+            }
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        val textView: TextView = findViewById(R.id.gpsText)
+        textView.text = "Latitude: ${location.latitude}, Longitude: ${location.longitude}"
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+    override fun onProviderEnabled(provider: String) {}
+    override fun onProviderDisabled(provider: String) {}
+
+    }
+
 
 
 
